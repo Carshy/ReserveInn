4ad2ef88d67cd052f6ae03734090a9f95f8429fc
carshy <mussocollins@gmail.com>
2024-07-15 07:07:04 +0300

    Add serializers for models

---
+++ reserverinn/bookings/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Location, Hotel, Room, Booking
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = ['id', 'city']
+
+class HotelSerializer(serializers.ModelSerializer):
+    location = LocationSerializer()
+
+    class Meta:
+        model = Hotel
+        fields = ['id', 'name', 'location', 'description']
+
+class RoomSerializer(serializers.ModelSerializer):
+    hotel = HotelSerializer()
+
+    class Meta:
+        model = Room
+        fields = ['id', 'hotel', 'room_type', 'number', 'price', 'available']
+
+class BookingSerializer(serializers.ModelSerializer):
+    user = User
+    room = RoomSerializer()
+
+    class Meta:
+        model = Booking
+        fields = ['id', 'user', 'room', 'check_in', 'check_out', 'booked_at']
