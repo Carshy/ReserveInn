423dbc6fe99d6851360a5826f337f4b016eba561
carshy <mussocollins@gmail.com>
2024-07-16 15:18:54 +0300

    Create frontend react and setup linters

---
+++ reserverinn-frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Collins Welcome back to react JS! This project is going to change your life</h1>
+    </div>
+  );
+}
+
+export default App;
