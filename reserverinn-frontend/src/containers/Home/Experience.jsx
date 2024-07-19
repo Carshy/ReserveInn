badab85d81660784fc5f9a5bbfffbdbc8db78c58
carshy <mussocollins@gmail.com>
2024-07-19 09:09:44 +0300

    Add background

--- reserverinn-frontend/src/containers/Home/Experience.jsx
+++ reserverinn-frontend/src/containers/Home/Experience.jsx
@@ -7,8 +7,8 @@ const Experience = () => {
     <div className="app__experience">
       <div className="app__experience-details">
         <h2>Shop Reserve Inn</h2>
-        <p>Experience the joy of Kempinski at home and share the delight with your loved ones with our collection of exclusive products and gift vouchers.</p>
-        <p>Luxuriate with sumptuous duvets and linens, indulge with divine bath and body products, or opt for a gift voucher so the special people in your life can savour Kempinski for themselves.</p>
+        <p>Experience the joy of Reserve Inn at home and share the delight with your loved ones with our collection of exclusive suites and luxury</p>
+        <p>Luxuriate with sumptuous duvets and linens, indulge with divine bath and body products, or opt for a gift voucher so the special people in your life can savour Reserve Inn for themselves.</p>
       </div>
       <div className="app__experience-image">
         <img src={images.kempi} alt="Experience"/>
